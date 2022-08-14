307305b86a0b8e1043a1f7abbcd9f34bb6d3b95e
juniortachom <constantjunior30@gmail.com>
2022-08-14 01:20:43 +0100

    databse heroku update
    Signed-off-by: juniortachom <constantjunior30@gmail.com>

--- config/database.php
+++ config/database.php
@@ -79,7 +79,7 @@ return [
             'prefix' => '',
             'prefix_indexes' => true,
             'schema' => 'public',
-            'sslmode' => 'prefer',
+            'sslmode' => 'require',
         ],
 
         'sqlsrv' => [
