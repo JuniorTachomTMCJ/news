44630e0951fcf75415931bb71bbcf926852293a7
juniortachom <constantjunior30@gmail.com>
2022-08-14 03:40:08 +0100

    add resource article for handle the urlToImage link

--- app/Http/Controllers/API/ArticleController.php
+++ app/Http/Controllers/API/ArticleController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\ArticleResource;
 use App\Models\Article;
 use App\Models\Category;
 use Illuminate\Http\Request;
@@ -25,7 +26,7 @@ class ArticleController extends Controller
             return response()->json(['message' => 'Internal server error', 500]);
         }
 
-        return response()->json($articles);
+        return response()->json(ArticleResource::collection($articles));
     }
 
     /**
@@ -72,7 +73,7 @@ class ArticleController extends Controller
             return response()->json(['message' => 'Internal server error', 500]);
         }
 
-        return response()->json($article, 201);
+        return response()->json(new ArticleResource($article), 201);
     }
 
     /**
---
+++ app/Http/Resources/ArticleResource.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ArticleResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        $array = parent::toArray($request);
+        $array['urlToImage'] = asset('storage/' . $this->urlToImage);
+        return $array;
+    }
+}
